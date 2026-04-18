b2d98e0410efbcd4942c0e62dfcf067034e2ddd9
wjdy-haun <qeryxjdhnnftyeji7r2gjy@gmail.com>
2026-04-18 23:23:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,85 @@
-let username = "";
+let users = JSON.parse(localStorage.getItem("users")) || {};
+let currentUser = null;
 let score = 0;
 
-let data = JSON.parse(localStorage.getItem("rank")) || {};
+// 회원가입
+function signup() {
+  let id = document.getElementById("id").value;
+  let pw = document.getElementById("pw").value;
+
+  if (!id || !pw) return alert("입력!");
+
+  users[id] = { pw: pw, score: 0 };
+
+  localStorage.setItem("users", JSON.stringify(users));
+
+  alert("회원가입 완료!");
+}
 
 // 로그인
 function login() {
-  username = document.getElementById("username").value;
+  let id = document.getElementById("id").value;
+  let pw = document.getElementById("pw").value;
 
-  if (!username) {
-    alert("이름 입력!");
-    return;
+  if (users[id] && users[id].pw === pw) {
+
+    currentUser = id;
+    score = users[id].score;
+
+    document.getElementById("auth").style.display = "none";
+    document.getElementById("game").style.display = "block";
+
+    document.getElementById("welcome").innerText = id + "님";
+
+    updateUI();
+  } else {
+    alert("로그인 실패");
   }
-
-  document.getElementById("login").style.display = "none";
-  document.getElementById("game").style.display = "block";
-
-  document.getElementById("welcome").innerText = username + "님 환영!";
 }
 
-// 점수 증가
-function addScore() {
+// 클릭
+function clickBox() {
   score++;
+
+  users[currentUser].score = score;
+
+  localStorage.setItem("users", JSON.stringify(users));
 
   document.getElementById("score").innerText = score;
 
-  data[username] = score;
+  // 50 단위 애니메이션
+  if (score % 50 === 0) {
+    let box = document.getElementById("box");
+    box.classList.add("pop");
 
-  localStorage.setItem("rank", JSON.stringify(data));
+    setTimeout(() => {
+      box.classList.remove("pop");
+    }, 300);
+  }
 
-  updateRank();
+  updateUI();
 }
 
-// 랭킹 업데이트
-function updateRank() {
-  let sorted = Object.entries(data).sort((a,b)=>b[1]-a[1]);
+// 랭킹 + 그래프
+function updateUI() {
 
+  let sorted = Object.entries(users).sort((a,b)=>b[1].score - a[1].score);
+
+  // 랭킹
   let html = "";
-
-  sorted.forEach((item, index) => {
-    let medal = "";
-
-    if (index === 0) medal = "🥇";
-    if (index === 1) medal = "🥈";
-    if (index === 2) medal = "🥉";
-
-    html += `<div>${medal} ${item[0]} : ${item[1]}</div>`;
+  sorted.forEach((u,i)=>{
+    html += (i+1)+"위 "+u[0]+" : "+u[1].score+"<br>";
   });
 
   document.getElementById("ranking").innerHTML = html;
+
+  // 그래프
+  let graph = "";
+
+  sorted.forEach(u=>{
+    graph += `<div>${u[0]}</div>
+              <div class="bar" style="width:${u[1].score}px"></div>`;
+  });
+
+  document.getElementById("graph").innerHTML = graph;
 }
